ecb152f92994ba65a2458aa873e77f781e524881
IgnFed <ignaciofedorenco2317@gmail.com>
2022-03-20 14:29:02 -0300

    Components added

--- components/P.tsx
+++ components/P.tsx
@@ -1,16 +1,6 @@
-import IBaseProps from "@/models/components/IBaseProps";
-import { Box } from "@chakra-ui/react";
+import styled from '@emotion/styled'
 
-export const P = <Props extends IBaseProps>(
-  {
-    children
-  }: Props
-)=>
-{
-  return(
-    <Box w='100%'>
-      <p>{children}</p>
-    </Box>
-  )
-
-}
+export const P = styled.p`
+  text-align: 'justify';
+  text-indent: 1rem;
+`
