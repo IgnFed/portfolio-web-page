e5548f536a7c2e71d07c3d213f5bd9fda6fd6a5b
IgnFed <ignaciofedorenco2317@gmail.com>
2022-03-20 14:40:21 -0300

    Changed wrong name

--- utils/nex-image-to-chakra.tsx
+++
@@ -1,6 +0,0 @@
-import { chakra } from "@chakra-ui/react";
-
-export default (NextImage:any)=> chakra(NextImage, {
-  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
-}
-)
