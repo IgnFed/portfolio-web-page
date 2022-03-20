bc6c2c60d44bbd4870d707fc07aa70241c6f66ba
IgnFed <ignaciofedorenco2317@gmail.com>
2022-03-20 14:30:18 -0300

    Created Projects Page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,10 +1,15 @@
 import { Layout } from "@/pages/Layout/article";
+import { Container, Heading } from "@chakra-ui/react";
 
 export default function Projects(){
   
   return(
     <Layout>
-      Projects
+      <Container mt={4}>
+        <Heading as={'h2'}>
+          
+        </Heading>
+      </Container>
     </Layout>
   )
 }
