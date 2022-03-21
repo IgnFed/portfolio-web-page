45565dcc60d731ed1efe2f91622823f08523c8df
IgnFed <ignaciofedorenco2317@gmail.com>
2022-03-21 03:42:49 -0300

    Removed unused utility function

--- utils/chakra-convert.ts
+++
@@ -1,8 +0,0 @@
-import { chakra, shouldForwardProp } from '@chakra-ui/react'
-
-
-export default (motionElement: any) => chakra(motionElement, {
-  shouldForwardProp: prop => {
-    return shouldForwardProp(prop) || prop === 'transition'
-  }
-})
