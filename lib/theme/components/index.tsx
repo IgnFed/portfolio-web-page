9867b0f81a73b038b38f434a0ded14b925f7898b
IgnFed <ignaciofedorenco2317@gmail.com>
2022-03-21 01:31:00 -0300

    Created new Heading Variant

--- lib/theme/components/index.tsx
+++ lib/theme/components/index.tsx
@@ -8,6 +8,17 @@ export const Heading = {
       marginTop: 3,
       marginBottom: 4,
       textUndelineColor: "#232323"
+    },
+    'underline-v2':{
+      textDecoration: 'underline',
+      fontSize: '4xl',
+      lineHeight: 1.2,
+      textUnderlineOffset: 6,
+      textDecorationThickness: 4,
+      marginTop: 3,
+      marginBottom: 4,
+      textUndelineColor: "#232323"
+
     }
   }
 }
